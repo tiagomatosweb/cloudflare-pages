270c632eb3093fe1d312bf8c14f99ba142f81245
Tiago Matos <tiago@tiagomatos.com>
2023-10-12 18:25:51 -0300

    Nuxt CF debug 4

--- pages/page1.vue
+++ pages/page1.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Page 1</h1>
     <br>
-    <pre>
+    <pre v-if="!pending">
       {{data}}
     </pre>
   </div>
@@ -11,5 +11,5 @@
 <script setup>
 import {useLazyAsyncData} from "#app";
 
-const { data } = useLazyAsyncData('page1', () => $fetch('https://staging-api.laravue.com.br/api/courses'))
+const { data, pending } = useLazyAsyncData('page1', () => $fetch('https://staging-api.laravue.com.br/api/courses'))
 </script>
