a373b97cc7ad6f3a490896175dbe75b92a632235
Tiago Matos <tiago@tiagomatos.com>
2023-10-12 18:39:41 -0300

    Nuxt CF debug 7

--- plugins/apiFetch.js
+++ plugins/apiFetch.js
@@ -1,15 +1,7 @@
-
 export default defineNuxtPlugin(() => {
-    const config = useRuntimeConfig()
-
     const apiFetch = $fetch.create({
         baseURL: 'https://jsonplaceholder.typicode.com',
         credentials: 'include',
-        headers: {
-            ...useRequestHeaders(['cookie']),
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-        },
     })
 
     return {
