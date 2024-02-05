1a8820bd4f92dd543c607d74920478e3a4b98d39
Tiago Matos <tiago@tiagomatos.com>
2024-02-05 14:28:08 +1100

    View port test 1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   devtools: { enabled: true },
+  app: {
+    head: {
+      viewport: 'width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;',
+      bodyAttrs: {
+        class: 'bg-neutral-100'
+      }
+    }
+  },
   runtimeConfig: {
     public: {
       apiURL: 'https://jsonplaceholder.typicode.com',
